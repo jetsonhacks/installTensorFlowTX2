10b022462ecbf10c534964cdee5530c3a182907a
jetsonhacks <jim@jetsonhacks.com>
2017-09-13 19:51:49 -0700

    Apply Github Checksum patch

--- cloneTensorFlow.sh
+++ cloneTensorFlow.sh
@@ -10,6 +10,8 @@ git clone https://github.com/tensorflow/tensorflow.git
 cd tensorflow
 git checkout v1.3.0
 patch -p1 < $INSTALL_DIR/patches/tensorflow.patch
+# Patch up the Workspace.bzl for the Github Checksum issue
+patch -p1 < $INSTALL_DIR/patches/workspacebzl.patch
 
 
 
