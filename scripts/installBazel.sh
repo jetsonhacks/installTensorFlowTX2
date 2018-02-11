5d3602d04709932e97fac31b0e5113662b6fa635
Stanley Nguyen <stanleynguyen@users.noreply.github.com>
2018-02-11 08:51:45 +0800

    Update Bazel to 0.10.0

--- scripts/installBazel.sh
+++ scripts/installBazel.sh
@@ -2,15 +2,15 @@
 # NVIDIA Jetson TX2
 # TensorFlow Installation
 # Install Bazel
-# Version 0.5.2 - v0.5.3 apparently has issues
+# Version 0.10.0
 # We use the release distribution so that we don't have to build protobuf
 #
 INSTALL_DIR=$PWD
 cd $HOME
-wget --no-check-certificate https://github.com/bazelbuild/bazel/releases/download/0.5.2/bazel-0.5.2-dist.zip
-unzip bazel-0.5.2-dist.zip -d bazel-0.5.2-dist
-sudo chmod -R ug+rwx $HOME/bazel-0.5.2-dist
+wget --no-check-certificate https://github.com/bazelbuild/bazel/releases/download/0.10.0/bazel-0.10.0-dist.zip
+unzip bazel-0.10.0-dist.zip -d bazel-0.10.0-dist
+sudo chmod -R ug+rwx $HOME/bazel-0.10.0-dist
 # git clone https://github.com/bazelbuild/bazel.git
-cd bazel-0.5.2-dist
+cd bazel-0.10.0-dist
 ./compile.sh 
 sudo cp output/bazel /usr/local/bin
